c4c8043e12724392ca29e28d145e8ede53f1708d
Anna Logacheva <anna.s.litvinenko@gmail.com>
2022-04-15 15:01:05 +0600

    Add styles to posts

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,10 +7,14 @@ const Post = (props) => {
             <div className={classes.avatar}>
                 <img src="https://images.unsplash.com/photo-1541840524505-3d825592d8ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" alt=""/>
             </div>
-            {props.message}
-            <div>
-                <button>Like </button>
-                {props.like_counts} likes
+            <div className={classes.flexCol}>
+                <div className={classes.postBox}>
+                    {props.message}
+                </div>
+                <div>
+                    <button>Like </button>
+                    {props.like_counts} likes
+                </div>
             </div>
         </div>
 
