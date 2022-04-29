15fe2073f0b6540b4a987550b5a644ed23eeebe1
Anna Logacheva <anna.s.litvinenko@gmail.com>
2022-04-29 17:15:26 +0600

    Added template of reselector

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,7 +1,11 @@
+import {createSelector} from "reselect";
+
 export const getUsers = (state) => {
     return state.usersPage.users;
 }
-
+// export const getUsersReselector = createSelector(getUsers,(users) => {
+//     return users.filter(u => true)
+// })
 export const getCurrentPage = (state) => {
     return state.usersPage.currentPage;
 }
